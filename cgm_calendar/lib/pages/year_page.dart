16902fd3f5b0b83b8cc8aafaf394caa76cd0517f
劉至秦 <liuzhiqin1990@gmail.com>
2022-07-11 22:59:59 +0900

    temp commit

--- cgm_calendar/lib/pages/year_page.dart
+++ cgm_calendar/lib/pages/year_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class YearPage extends StatefulWidget {
   YearPage({Key? key}) : super(key: key);
@@ -19,6 +20,20 @@ class _YearPageState extends State<YearPage> {
     "2025",
     "2026",
   ];
+  int year = 0;
+  bool needCalculate = false;
+  ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final contentSize = controller.position.viewportDimension +
+          controller.position.maxScrollExtent;
+      final target = contentSize * 4 / data.length;
+      controller.jumpTo(target);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,14 +94,61 @@ class _YearPageState extends State<YearPage> {
       ),
       body: NotificationListener<ScrollNotification>(
         onNotification: (notification) {
+          if (notification.metrics.pixels < 10) {
+            if (!needCalculate) {
+              return true;
+            }
+
+            String first = data[0];
+            year = int.parse(first);
+            List<String> newData = [
+              (year - 4).toString(),
+              (year - 3).toString(),
+              (year - 2).toString(),
+              (year - 1).toString(),
+            ];
+            newData.addAll(data);
+            data = newData;
+          } else if (notification.metrics.pixels >
+              (notification.metrics.maxScrollExtent - 100)) {
+            if (!needCalculate) {
+              return true;
+            }
+            String last = data[data.length - 1];
+            year = int.parse(last);
+            List<String> newData = [
+              (year + 1).toString(),
+              (year + 2).toString(),
+              (year + 3).toString(),
+              (year + 4).toString(),
+            ];
+            data.addAll(newData);
+          } else {
+            needCalculate = true;
+            return true;
+          }
+
+          needCalculate = false;
+          setState(() {
+            data;
+            final contentSize = controller.position.viewportDimension +
+                controller.position.maxScrollExtent;
+            final target = contentSize * 4 / data.length;
+            controller.jumpTo(target + controller.offset);
+          });
           debugPrint("scroll : ${notification.metrics.pixels}");
           debugPrint("max : ${notification.metrics.maxScrollExtent}");
           return true;
         },
         child: ListView.builder(
+          controller: controller,
           itemCount: data.length,
           itemBuilder: ((context, index) {
             return Container(
+              height: 200,
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(color: Colors.black, width: 1))),
               margin: const EdgeInsets.all(10),
               child: Text(data[index]),
             );
