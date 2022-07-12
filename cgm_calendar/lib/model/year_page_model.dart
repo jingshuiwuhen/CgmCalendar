42d35cac4d085e813c1b9dcc518ee786ec21abbf
劉至秦 <liuzhiqin1990@gmail.com>
2022-07-12 23:48:10 +0900

    temp commit

--- cgm_calendar/lib/model/year_page_model.dart
+++ cgm_calendar/lib/model/year_page_model.dart
@@ -2,32 +2,48 @@ import 'package:cgm_calendar/model/date_model.dart';
 import 'package:flutter/material.dart';
 
 class YearPageModel with ChangeNotifier {
-  static const int _length = 20;
+  static const int _length = 10;
 
   final List<DateModel> _oldYears = [];
   final List<DateModel> _newYears = [];
 
   YearPageModel() {
-    _initData();
+    initData();
   }
 
   List<DateModel> get oldYears => _oldYears;
   List<DateModel> get newYears => _newYears;
 
-  void _initData() {
-    int yearOfNow = DateTime.now().year;
-    addOldYears(yearOfNow - 1);
-    addNewYears(yearOfNow);
+  void initData() {
+    oldYears.clear();
+    newYears.clear();
+    _addOldYears();
+    _addNewYears();
+    notifyListeners();
   }
 
-  void addOldYears(int lastYear) {
+  void addData() {
+    _addOldYears();
+    _addNewYears();
+    notifyListeners();
+  }
+
+  void _addOldYears() {
+    int lastYear = DateTime.now().year - 1;
+    if (oldYears.isNotEmpty) {
+      lastYear = oldYears.last.year - 1;
+    }
     for (var i = 0; i < _length; i++) {
       var dateModel = DateModel(lastYear - i);
       oldYears.add(dateModel);
     }
   }
 
-  void addNewYears(int beginYear) {
+  void _addNewYears() {
+    int beginYear = DateTime.now().year;
+    if (newYears.isNotEmpty) {
+      beginYear = newYears.last.year + 1;
+    }
     for (var i = 0; i < _length; i++) {
       var dateModel = DateModel(beginYear + i);
       newYears.add(dateModel);
--- cgm_calendar/lib/pages/year_page.dart
+++ cgm_calendar/lib/pages/year_page.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 
 class YearPage extends StatelessWidget {
   bool _needCalculate = false;
+  final Key _centerKey = const ValueKey("second-sliver-list");
 
   YearPage({Key? key}) : super(key: key);
 
@@ -64,17 +65,82 @@ class YearPage extends StatelessWidget {
           ),
         ),
       ),
-      body: ChangeNotifierProvider(
+      body: ChangeNotifierProvider<YearPageModel>(
         create: (_) => YearPageModel(),
-        child: NotificationListener<ScrollNotification>(
+        builder: (context, child) => NotificationListener<ScrollNotification>(
           onNotification: (notification) {
+            debugPrint(
+                "viewportDimension : ${notification.metrics.viewportDimension}");
+            debugPrint("scroll : ${notification.metrics.pixels}");
+            debugPrint("max : ${notification.metrics.maxScrollExtent}");
+            if (notification.metrics.pixels >
+                (notification.metrics.maxScrollExtent - 100)) {
+              if (_needCalculate) {
+                _needCalculate = false;
+                Provider.of<YearPageModel>(context, listen: false).addData();
+              }
+            } else if (notification.metrics.viewportDimension +
+                    notification.metrics.maxScrollExtent +
+                    notification.metrics.pixels <
+                100) {
+              if (_needCalculate) {
+                _needCalculate = false;
+                Provider.of<YearPageModel>(context, listen: false).addData();
+              }
+            } else {
+              _needCalculate = true;
+            }
+
             return true;
           },
           child: CustomScrollView(
+            center: _centerKey,
             slivers: [
               SliverList(
-                delegate: SliverChildBuilderDelegate(),
-              )
+                delegate: SliverChildBuilderDelegate(
+                  ((context, index) {
+                    return Container(
+                      height: 100,
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom:
+                                  BorderSide(color: Colors.black, width: 1))),
+                      margin: const EdgeInsets.all(10),
+                      child: Text(
+                        context
+                            .watch<YearPageModel>()
+                            .oldYears[index]
+                            .year
+                            .toString(),
+                      ),
+                    );
+                  }),
+                  childCount: context.watch<YearPageModel>().oldYears.length,
+                ),
+              ),
+              SliverList(
+                key: _centerKey,
+                delegate: SliverChildBuilderDelegate(
+                  ((context, index) {
+                    return Container(
+                      height: 100,
+                      decoration: const BoxDecoration(
+                          border: Border(
+                              bottom:
+                                  BorderSide(color: Colors.black, width: 1))),
+                      margin: const EdgeInsets.all(10),
+                      child: Text(
+                        context
+                            .watch<YearPageModel>()
+                            .newYears[index]
+                            .year
+                            .toString(),
+                      ),
+                    );
+                  }),
+                  childCount: context.watch<YearPageModel>().newYears.length,
+                ),
+              ),
             ],
           ),
         ),
@@ -83,160 +149,160 @@ class YearPage extends StatelessWidget {
   }
 }
 
-class YearPage extends StatefulWidget {
-  YearPage({Key? key}) : super(key: key);
+// class YearPage extends StatefulWidget {
+//   YearPage({Key? key}) : super(key: key);
 
-  @override
-  State<StatefulWidget> createState() => _YearPageState();
-}
+//   @override
+//   State<StatefulWidget> createState() => _YearPageState();
+// }
 
-class _YearPageState extends State<YearPage> {
-  List<String> data = [
-    "2018",
-    "2019",
-    "2020",
-    "2021",
-    "2022",
-    "2023",
-    "2024",
-    "2025",
-    "2026",
-  ];
-  int year = 0;
-  bool needCalculate = false;
-  ScrollController controller = ScrollController();
+// class _YearPageState extends State<YearPage> {
+//   List<String> data = [
+//     "2018",
+//     "2019",
+//     "2020",
+//     "2021",
+//     "2022",
+//     "2023",
+//     "2024",
+//     "2025",
+//     "2026",
+//   ];
+//   int year = 0;
+//   bool needCalculate = false;
+//   ScrollController controller = ScrollController();
 
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final contentSize = controller.position.viewportDimension +
-          controller.position.maxScrollExtent;
-      final target = contentSize * 4 / data.length;
-      controller.jumpTo(target);
-    });
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       final contentSize = controller.position.viewportDimension +
+//           controller.position.maxScrollExtent;
+//       final target = contentSize * 4 / data.length;
+//       controller.jumpTo(target);
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.red,
-        elevation: 1,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.add,
-            ),
-          )
-        ],
-      ),
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              blurRadius: 1,
-            )
-          ],
-        ),
-        child: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.calendar_today,
-                  color: Colors.red,
-                ),
-              ),
-              const Spacer(),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.menu,
-                  color: Colors.red,
-                ),
-              ),
-              const Spacer(),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.info_outline,
-                  color: Colors.red,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: NotificationListener<ScrollNotification>(
-        onNotification: (notification) {
-          if (notification.metrics.pixels < 10) {
-            if (!needCalculate) {
-              return true;
-            }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.white,
+//         foregroundColor: Colors.red,
+//         elevation: 1,
+//         actions: [
+//           IconButton(
+//             onPressed: () {},
+//             icon: const Icon(
+//               Icons.add,
+//             ),
+//           )
+//         ],
+//       ),
+//       bottomNavigationBar: Container(
+//         decoration: const BoxDecoration(
+//           color: Colors.white,
+//           boxShadow: [
+//             BoxShadow(
+//               color: Colors.grey,
+//               blurRadius: 1,
+//             )
+//           ],
+//         ),
+//         child: SafeArea(
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               IconButton(
+//                 onPressed: () {},
+//                 icon: const Icon(
+//                   Icons.calendar_today,
+//                   color: Colors.red,
+//                 ),
+//               ),
+//               const Spacer(),
+//               IconButton(
+//                 onPressed: () {},
+//                 icon: const Icon(
+//                   Icons.menu,
+//                   color: Colors.red,
+//                 ),
+//               ),
+//               const Spacer(),
+//               IconButton(
+//                 onPressed: () {},
+//                 icon: const Icon(
+//                   Icons.info_outline,
+//                   color: Colors.red,
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//       body: NotificationListener<ScrollNotification>(
+//         onNotification: (notification) {
+//           if (notification.metrics.pixels < 10) {
+//             if (!needCalculate) {
+//               return true;
+//             }
 
-            String first = data[0];
-            year = int.parse(first);
-            List<String> newData = [
-              (year - 4).toString(),
-              (year - 3).toString(),
-              (year - 2).toString(),
-              (year - 1).toString(),
-            ];
-            newData.addAll(data);
-            data = newData;
-          } else if (notification.metrics.pixels >
-              (notification.metrics.maxScrollExtent - 100)) {
-            if (!needCalculate) {
-              return true;
-            }
-            String last = data[data.length - 1];
-            year = int.parse(last);
-            List<String> newData = [
-              (year + 1).toString(),
-              (year + 2).toString(),
-              (year + 3).toString(),
-              (year + 4).toString(),
-            ];
-            data.addAll(newData);
-          } else {
-            needCalculate = true;
-            return true;
-          }
+//             String first = data[0];
+//             year = int.parse(first);
+//             List<String> newData = [
+//               (year - 4).toString(),
+//               (year - 3).toString(),
+//               (year - 2).toString(),
+//               (year - 1).toString(),
+//             ];
+//             newData.addAll(data);
+//             data = newData;
+//           } else if (notification.metrics.pixels >
+//               (notification.metrics.maxScrollExtent - 100)) {
+//             if (!needCalculate) {
+//               return true;
+//             }
+//             String last = data[data.length - 1];
+//             year = int.parse(last);
+//             List<String> newData = [
+//               (year + 1).toString(),
+//               (year + 2).toString(),
+//               (year + 3).toString(),
+//               (year + 4).toString(),
+//             ];
+//             data.addAll(newData);
+//           } else {
+//             needCalculate = true;
+//             return true;
+//           }
 
-          needCalculate = false;
-          setState(() {
-            data;
-            final contentSize = controller.position.viewportDimension +
-                controller.position.maxScrollExtent;
-            final target = contentSize * 4 / data.length;
-            controller.jumpTo(target + controller.offset);
-          });
-          debugPrint("scroll : ${notification.metrics.pixels}");
-          debugPrint("max : ${notification.metrics.maxScrollExtent}");
-          return true;
-        },
-        child: ListView.builder(
-          controller: controller,
-          itemCount: data.length,
-          itemBuilder: ((context, index) {
-            return Container(
-              height: 200,
-              decoration: const BoxDecoration(
-                  border: Border(
-                      bottom: BorderSide(color: Colors.black, width: 1))),
-              margin: const EdgeInsets.all(10),
-              child: Text(data[index]),
-            );
-          }),
-        ),
-      ),
-    );
-  }
-}
+//           needCalculate = false;
+//           setState(() {
+//             data;
+//             final contentSize = controller.position.viewportDimension +
+//                 controller.position.maxScrollExtent;
+//             final target = contentSize * 4 / data.length;
+//             controller.jumpTo(target + controller.offset);
+//           });
+//           debugPrint("scroll : ${notification.metrics.pixels}");
+//           debugPrint("max : ${notification.metrics.maxScrollExtent}");
+//           return true;
+//         },
+//         child: ListView.builder(
+//           controller: controller,
+//           itemCount: data.length,
+//           itemBuilder: ((context, index) {
+//             return Container(
+//               height: 200,
+//               decoration: const BoxDecoration(
+//                   border: Border(
+//                       bottom: BorderSide(color: Colors.black, width: 1))),
+//               margin: const EdgeInsets.all(10),
+//               child: Text(data[index]),
+//             );
+//           }),
+//         ),
+//       ),
+//     );
+//   }
+// }
